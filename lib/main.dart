537ddfa1763e3c7c1819d2d0ee5c377484f0988f
Hung Pham Sy <hungps.work@gmail.com>
2023-01-14 18:42:46 +0700

    feat: setting up dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_config/flutter_config.dart';
+import 'package:flutter_starter/di.dart';
 import 'package:flutter_starter/presenter/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await FlutterConfig.loadEnvVariables();
+
+  configureDependencies();
 
   runApp(const App());
 }
