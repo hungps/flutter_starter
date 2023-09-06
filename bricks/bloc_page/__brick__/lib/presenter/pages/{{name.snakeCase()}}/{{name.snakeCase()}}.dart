b1cb05b22511b7d8a3e6a9d22dfa4b951286fd99
Hung Pham Sy <hungps.work@gmail.com>
2023-09-06 17:51:44 +0700

    chore: adding bloc_page todo

--- bricks/bloc_page/__brick__/lib/presenter/pages/{{name.snakeCase()}}/{{name.snakeCase()}}.dart
+++ bricks/bloc_page/__brick__/lib/presenter/pages/{{name.snakeCase()}}/{{name.snakeCase()}}.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_starter/di.dart';
+import 'package:flutter_starter/presenter/pages/{{name.snakeCase()}}/{{name.snakeCase()}}_bloc.dart';
 
-class {{name.pascalCase()}}Page extends StatelessWidget {
+// TODO: Add the page to the router
+@RoutePage()
+class {{name.pascalCase()}}Page extends StatefulWidget {
+  @override
+  State<{{name.pascalCase()}}Page> createState() => _{{name.pascalCase()}}PageState();
+}
+
+class _{{name.pascalCase()}}PageState extends State<{{name.pascalCase()}}Page> {
+  // TODO: Register the bloc in the provider
+  final {{name.pascalCase()}}Bloc _bloc = provider.get<{{name.pascalCase()}}Bloc>();
+
+  @override
+  void dispose() {
+    _bloc.close();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('{{name.titleCase()}} Page'),
-      ),
-      body: const Center(
-        child: Text('{{name.titleCase()}} Page'),
+    return BlocProvider.value(
+      value: _bloc,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('{{name.titleCase()}} Page'),
+        ),
+        body: const Center(
+          child: Text('{{name.titleCase()}} Page'),
+        ),
       ),
     );
   }
