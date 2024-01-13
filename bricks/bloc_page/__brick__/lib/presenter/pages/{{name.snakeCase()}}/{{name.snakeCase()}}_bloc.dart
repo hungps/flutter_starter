839caf0d11816b2ce3538ce188cd2138f333e3e5
Hung Pham Sy <hungps.work@gmail.com>
2024-01-13 19:42:10 +0700

    fix: add @injectable annotation to base bloc

--- bricks/bloc_page/__brick__/lib/presenter/pages/{{name.snakeCase()}}/{{name.snakeCase()}}_bloc.dart
+++ bricks/bloc_page/__brick__/lib/presenter/pages/{{name.snakeCase()}}/{{name.snakeCase()}}_bloc.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
 import 'package:flutter_starter/core/exception.dart';
 import 'package:flutter_starter/presenter/pages/{{name.snakeCase()}}/{{name.snakeCase()}}_event.dart';
 import 'package:flutter_starter/presenter/pages/{{name.snakeCase()}}/{{name.snakeCase()}}_state.dart';
 
+@injectable
 class {{name.pascalCase()}}Bloc extends Bloc<{{name.pascalCase()}}Event, {{name.pascalCase()}}State> {
   {{name.pascalCase()}}Bloc() : super(const {{name.pascalCase()}}State()) {
     on<{{name.pascalCase()}}ErrorOccurred>(_onErrorOccurred);
